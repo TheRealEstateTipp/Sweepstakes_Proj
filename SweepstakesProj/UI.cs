adc420a4fdb59f81f2629168b9bf533d1f81b71c
unknown <mstiff1983@gmail.com>
2020-06-02 11:14:21 -0400

    I have created a UI static class that contains a PrintString method and a GetUserInput method

---
+++ SweepstakesProj/UI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProj
+{
+    public static class UI
+    {
+        public static void PrintString(string output)
+        {
+            Console.WriteLine(output);
+        }
+
+        public static string GetUserInput(string output)
+        {
+            Console.WriteLine(output);
+            string input = Console.ReadLine();
+            return input;
+        }
+    }
+}

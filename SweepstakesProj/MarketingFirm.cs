4d5645571b2483c3b60a78dac0bc758c0e60bb16
unknown <mstiff1983@gmail.com>
2020-06-03 12:22:23 -0400

    I have used dependency injection in the constructor of the MarketingFirm which will allow to choose which type of manager the MarketingFirm would like to use.

--- SweepstakesProj/MarketingFirm.cs
+++ SweepstakesProj/MarketingFirm.cs
@@ -6,18 +6,26 @@ using System.Threading.Tasks;
 
 namespace SweepstakesProj
 {
-    class MarketingFirm
+    class MarketingFirm 
     {
         ISweepstakesManager manager;
 
         public MarketingFirm(ISweepstakesManager manager)
         {
+            this.manager = manager;
+
+            //We have used dependency injection here in the constructor method because this will allow the marketing firm to use Sweepstakes manager,
+            //but it also allows the Marketing Firm to choose which type of manager that it would like to use based on the classes that took on the 
+            //interface of the ISweepstakesManager, which are SweepstakesQueueManager and the SweepstakesStackManager. The Marketing Firm now can determine
+            //if they would like to use the FIFO method with the Queue or the FIFO method with the Stack but it will be their choice.
 
         }
 
         public void CreateSweepstake()
         {
-
+            Sweepstakes newSweepstake = new Sweepstakes("newSweepstake");
         }
+
     }
+
 }

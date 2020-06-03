bbb9200c5d2076b3dd346f72d9b85cb1f6f1e833
unknown <mstiff1983@gmail.com>
2020-06-03 11:26:48 -0400

    I have added implemented an added the logic to the InsertSweepstakes method and the GetSweepstakes method on the SweepstakesQueue manager class that uses the ISweepstakesManager interface.

--- SweepstakesProj/SweepstakesQueueManager.cs
+++ SweepstakesProj/SweepstakesQueueManager.cs
@@ -12,12 +12,17 @@ namespace SweepstakesProj
 
         public void InsertSweepstakes(Sweepstakes sweepstakes)
         {
-
+            queue = new Queue<Sweepstakes>();
+            queue.Enqueue(sweepstakes);
         }
 
         public Sweepstakes GetSweepstakes()
         {
+            queue = new Queue<Sweepstakes>();
+            Sweepstakes sweepstakes = queue.Dequeue();
 
+            UI.PrintString("The sweepstake to be removed from the queue is" + sweepstakes);
+            return sweepstakes;
         }
 
 
